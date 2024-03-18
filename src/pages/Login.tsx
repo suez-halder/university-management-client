02ec1c99718b502cf5a7c629f9ae6c60ba5310ca
suezhalder <szhldr@gmail.com>
2024-03-18 19:12:18 +0600

    login working

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,34 @@
+import { Button } from "antd";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+
+    const [login, { data, error }] = useLoginMutation();
+
+    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+        const userInfo = {
+            id: data.userId,
+            password: data.password,
+        };
+
+        const res = await login(userInfo);
+        console.log(res);
+    };
+
     return (
-        <div>
-            <h2>Welcome to the Login page</h2>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="id">ID: </label>
+                <input type="text" id="id" {...register("userId")} />
+            </div>
+            <div>
+                <label htmlFor="password">Password: </label>
+                <input type="text" id="password" {...register("password")} />
+            </div>
+            <Button htmlType="submit">Login</Button>
+        </form>
     );
 };
 
