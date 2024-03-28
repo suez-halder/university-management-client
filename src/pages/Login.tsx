4cda94b02fa21d1cfddc698f1117706a71bb88ca
suezhalder <szhldr@gmail.com>
2024-03-28 23:54:57 +0600

    custom form and input created

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,14 @@
-import { Button } from "antd";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { Button, Row } from "antd";
+import {
+    FieldValues,
+    SubmitHandler,
+    useForm,
+    useFormContext,
+} from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import UMForm from "../components/form/UMForm";
+import UMInput from "../components/form/UMInput";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { useAppDispatch } from "../redux/hooks";
@@ -10,16 +17,22 @@ import { verifyToken } from "../utils/verifyToken";
 const Login = () => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
-    const { register, handleSubmit } = useForm({
-        defaultValues: {
-            userId: "A-0001",
-            password: "admin123",
-        },
-    });
+    // const { register, handleSubmit } = useForm({
+    //     defaultValues: {
+    //         userId: "A-0001",
+    //         password: "admin123",
+    //     },
+    // });
+
+    const defaultValues = {
+        userId: "A-0001",
+        password: "admin123",
+    };
 
     const [login] = useLoginMutation();
 
     const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+        console.log(data);
         const toastId = toast.loading("Logging in..");
 
         try {
@@ -55,17 +68,15 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="id">ID: </label>
-                <input type="text" id="id" {...register("userId")} />
-            </div>
-            <div>
-                <label htmlFor="password">Password: </label>
-                <input type="text" id="password" {...register("password")} />
-            </div>
-            <Button htmlType="submit">Login</Button>
-        </form>
+        <Row justify="center" align="middle" style={{ height: "100vh" }}>
+            <UMForm onSubmit={onSubmit} defaultValues={defaultValues}>
+                <UMInput type="text" name="userId" label="ID: " />
+
+                <UMInput type="text" name="password" label="Password: " />
+
+                <Button htmlType="submit">Login</Button>
+            </UMForm>
+        </Row>
     );
 };
 
